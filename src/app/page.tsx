9537a18495159434d675977dd5bb07e7945757fa
Darrion Dumlao <darrion@Darrions-MBP.hsd1.ca.comcast.net>
2023-08-09 14:58:14 -0700

    added todo function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+'use client'
+import React from 'react'
+
 export default function Home() {
-	return 'Hello World'
+	const [todos, setTodos] = React.useState([])
+	const todoInput = React.useRef(null)
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		setTodos([...todos, todoInput.current.value])
+		todoInput.current.value = ''
+	}
+
+	const handleDelete = (indexToDelete) => {
+		setTodos(todos.filter((_, index) => index !== indexToDelete))
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label>
+					New ToDo:
+					<input ref={todoInput} type='text' placeholder='type here...' />
+				</label>
+				<button type='submit'>Submit</button>
+			</form>
+			<div>
+				{todos.map((todo, index) => (
+					<div key={index}>
+						{todo}
+						<button onClick={() => handleDelete(index)}>Delete</button>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
